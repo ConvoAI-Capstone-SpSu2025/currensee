1074b2ed9e1867ecd5b32a0f9e67cde65073a7a7
Stepan Ochodek <sochodek@uchicago.edu>
2025-04-30 03:45:07 +0000

    adjusting fin tools for basic database query

--- currensee/core/settings.py
+++ currensee/core/settings.py
@@ -30,7 +30,6 @@ def check_str_is_http(x: str) -> str:
     http_url_adapter = TypeAdapter(HttpUrl)
     return str(http_url_adapter.validate_python(x))
 
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=find_dotenv(),
@@ -40,34 +39,26 @@ class Settings(BaseSettings):
         validate_default=False,
     )
     MODE: str | None = None
-
     HOST: str = "0.0.0.0"
     PORT: int = 8080
-
     AUTH_SECRET: SecretStr | None = None
-
     GOOGLE_API_KEY: SecretStr | None = None
     USE_FAKE_MODEL: bool = False
-
     # If DEFAULT_MODEL is None, it will be set in model_post_init
     DEFAULT_MODEL: AllModelEnum | None = GoogleModelName.GEMINI_15_FLASH  # type: ignore[assignment]
     AVAILABLE_MODELS: set[AllModelEnum] = set()  # type: ignore[assignment]
-
     OPENWEATHERMAP_API_KEY: SecretStr | None = None
-
     LANGCHAIN_TRACING_V2: bool = False
     LANGCHAIN_PROJECT: str = "default"
     LANGCHAIN_ENDPOINT: Annotated[str, BeforeValidator(check_str_is_http)] = (
         "https://api.smith.langchain.com"
     )
     LANGCHAIN_API_KEY: SecretStr | None = None
-
     # Database Configuration
     DATABASE_TYPE: DatabaseType = (
         DatabaseType.SQLITE
     )  # Options: DatabaseType.SQLITE or DatabaseType.POSTGRES
     SQLITE_DB_PATH: str = "checkpoints.db"
-
     # PostgreSQL Configuration
     POSTGRES_USER: str | None = None
     POSTGRES_PASSWORD: SecretStr | None = None
@@ -81,17 +72,18 @@ class Settings(BaseSettings):
         default=3, description="Minimum number of connections in the pool"
     )
     POSTGRES_MAX_IDLE: int = Field(default=5, description="Maximum number of idle connections")
-
+    @computed_field
+    @property
+    def POSTGRES_ENGINE_STR(self) -> str:
+        return f'postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD.get_secret_value()}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}'
     def model_post_init(self, __context: Any) -> None:
         api_keys = {
             Provider.GOOGLE: self.GOOGLE_API_KEY,
             Provider.FAKE: self.USE_FAKE_MODEL,
         }
-
         active_keys = [k for k, v in api_keys.items() if v]
         if not active_keys:
             raise ValueError("At least one LLM API key must be provided.")
-
         for provider in active_keys:
             match provider:
                 case Provider.GOOGLE:
@@ -102,17 +94,12 @@ class Settings(BaseSettings):
                     if self.DEFAULT_MODEL is None:
                         self.DEFAULT_MODEL = FakeModelName.FAKE
                     self.AVAILABLE_MODELS.update(set(FakeModelName))
-     
                 case _:
                     raise ValueError(f"Unknown provider: {provider}")
-
     @computed_field
     @property
     def BASE_URL(self) -> str:
         return f"http://{self.HOST}:{self.PORT}"
-
-    def is_dev(self) -> bool:
-        return self.MODE == "dev"
 
 
 settings = Settings()
