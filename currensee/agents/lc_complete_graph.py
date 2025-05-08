d8cb56decc1a52057822ae9924dcdcaba8da5f83
lilycampbell0 <lilycampbell@uchicago.edu>
2025-05-08 05:14:02 +0000

    add testing files for LC of recent email agent

---
+++ currensee/agents/lc_complete_graph.py
@@ -0,0 +1,114 @@
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START, END
+
+from currensee.core import get_model, settings
+from currensee.agents.agent_utils import summarize_outputs, ReturnAsyncValue
+from currensee.agents.tools.base import SupervisorState
+
+from currensee.agents.tools.crm_tools import retrieve_client_metadata
+from currensee.agents.tools.outlook_tools import produce_client_email_summary
+from currensee.agents.tools.outlook_tools_lc import produce_recent_client_email_summary
+from currensee.agents.tools.finance_tools import retrieve_client_industry_news, retrieve_holdings_news, retrieve_macro_news, summarize_finance_outputs
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import asyncio
+
+# === Model ===
+model = get_model(settings.DEFAULT_MODEL)
+
+
+# === Summary ===
+
+def summarize_all_outputs(state: SupervisorState) -> str:
+    """
+    Summarizes the outputs from all provided tools into one coherent summary.
+    
+    Parameters:
+    - tool_outputs: A list of strings (outputs from different tools)
+    
+    Returns:
+    - A summarized string with key points from all the tool outputs.
+    """
+
+    finance_summary = state["finnews_summary"]
+    email_summary = state["email_summary"]
+    recent_email_summary = state["recent_email_summary"]
+    company_name = state['client_company']
+    client_name = state['client_name']
+    meeting_description = state['meeting_description']
+
+    # Combine all outputs into a formatted prompt
+    combined_prompt = f"""
+        You are a skilled financial advisor with an upcoming meeting with {client_name} who works for {company_name}. Below are summaries of email correspondence with that client and the relevant financial data regarding the recent company's industry performance, the performance of their stock holdings, and the performance of the overall economy.
+
+        Combine these summaries into one document that will help prepare other meeting attendees for the meeting with all of the relevant data, keeping in mind that the topic of the meeting is {meeting_description}. Format into multiple paragraphs with separate sections (with headings) for the past meeting/email summary, most recent email summary, and the financial news data.
+
+        email summary : {email_summary}
+
+        recent email summary : {recent_email_summary}
+
+        financial news summary: {finance_summary}
+
+
+    """
+    
+    # Create the messages to pass to the model
+    messages = [
+        HumanMessage(content=combined_prompt)
+    ]
+    
+    # Use the 'invoke' method for summarization
+    summary = model.invoke(messages)
+
+    new_state = state.copy()
+    new_state["final_summary"] = summary.content
+    
+    # Access the message content correctly
+    return new_state
+
+# === Build the Graph ===
+
+
+# Define the multi-agent supervisor graph
+complete_graph = StateGraph(SupervisorState)
+
+complete_graph.add_node("retrieve_client_metadata", retrieve_client_metadata)
+complete_graph.add_node("produce_outlook_summary", produce_client_email_summary)
+complete_graph.add_node("produce_recent_outlook_summary", produce_recent_client_email_summary)
+
+
+complete_graph.add_node("run_client_holdings_agent", retrieve_holdings_news)
+complete_graph.add_node("run_client_industry_agent", retrieve_client_industry_news)
+complete_graph.add_node("run_macro_finnews_agent", retrieve_macro_news)
+complete_graph.add_node("finance_summarizer_agent", summarize_finance_outputs)
+complete_graph.add_node("final_summarizer_agent", summarize_all_outputs)
+
+complete_graph.add_edge(START, "retrieve_client_metadata")
+complete_graph.add_edge("retrieve_client_metadata", "produce_outlook_summary")
+complete_graph.add_edge("produce_outlook_summary", "produce_recent_outlook_summary")
+complete_graph.add_edge("produce_recent_outlook_summary", "run_macro_finnews_agent")
+complete_graph.add_edge("run_macro_finnews_agent", "run_client_industry_agent")
+complete_graph.add_edge("run_client_industry_agent", "run_client_holdings_agent")
+complete_graph.add_edge("run_client_holdings_agent", "finance_summarizer_agent")
+complete_graph.add_edge("finance_summarizer_agent", "final_summarizer_agent")
+complete_graph.add_edge("final_summarizer_agent", END)
+
+compiled_graph = complete_graph.compile()
+
+def main():
+    init_state = {
+        'client_name': 'Adam Clay',
+        'client_email': 'adam.clay@compass.com',
+        'meeting_timestamp': '2024-03-26 11:00:00',
+        'meeting_description': 'Compass - Annual Credit Facility Review Meeting'
+
+    }
+
+    result = compiled_graph.invoke(init_state)
+
+    print(result['finnews_summary'])
+
+if __name__ == "__main__":
+    main()
---
+++ currensee/agents/tools/outlook_tools_lc.py
@@ -0,0 +1,191 @@
+import pandas as pd
+import numpy as np
+from currensee.utils.db_utils import create_pg_engine
+from sqlalchemy import text
+from langchain_core.messages import HumanMessage
+from currensee.workflows.sql_workflow.utils import create_sql_workflow
+from currensee.workflows.workflow_descriptions import outlook_table_description_mapping
+from currensee.core import get_model, settings
+from currensee.agents.tools.base import SupervisorState
+from dotenv import load_dotenv
+load_dotenv()
+
+#description of 2 most recent emails
+
+# === Model ===
+model = get_model(settings.DEFAULT_MODEL)
+
+
+# === DB Connection ===
+DB_NAME = 'crm_outlook'
+engine = create_pg_engine(
+   db_name=DB_NAME
+)
+
+# sql_workflow = create_sql_workflow(
+#     source_db = DB_NAME,
+#     table_description_mapping=outlook_table_description_mapping,
+#     synthesize_response=False
+# )
+
+
+# async def find_last_meeting_date(all_client_emails) -> dict:
+
+#     query_str = f"When was the date of the last meeting that was conducted with any of the following emails: {all_client_emails}?"
+
+#     result = await sql_workflow.run(query=query_str)
+
+#     return result.response
+
+def find_last_meeting_date(all_client_emails: list[str]) -> dict:
+    """
+        Find the last meeting date that was held with any of the 
+        client emails listed above.
+    """
+
+    query_str = f"""
+        SELECT  meeting_timestamp
+        FROM  meeting_data
+        WHERE  invitee_emails ~* '{'|'.join(all_client_emails)}' 
+        ORDER BY meeting_timestamp DESC
+        LIMIT 1;
+
+    """
+
+    last_meeting = pd.read_sql(query_str, con=engine)
+
+    return last_meeting['meeting_timestamp'][0]
+
+
+def produce_client_email_summary(state: SupervisorState) -> dict:
+    """
+        Produce a client email summary from the most recent and
+        relevant emails based on the email description.
+    """
+    client_company = state['client_company']
+    all_client_emails = state['all_client_emails']
+    meeting_timestamp = state['meeting_timestamp']
+    meeting_description = state['meeting_description']
+
+    last_meeting_date = find_last_meeting_date(all_client_emails=all_client_emails)
+
+    ## NOTE: I currently removed the meeting timestamp restrictions, because 
+    ## it wasn't returning any emails.
+    
+    query_str = f"""
+        SELECT email_body
+        FROM email_data
+        WHERE 
+        -- email_timestamp <= '{meeting_timestamp}' AND email_timestamp >= '{last_meeting_date}'
+        -- AND 
+        (
+            to_emails ~* '{'|'.join(all_client_emails)}' 
+            OR 
+            from_email ~* '{'|'.join(all_client_emails)}' 
+        )
+    """
+
+    result = pd.read_sql(query_str, con=engine)
+
+    recent_emails = list(result['email_body'])
+
+    recent_email_str = '\n'.join(recent_emails)
+
+    ###### SUMMARIZE HERE #########
+
+    summary_prompt = f"""
+        PROMPT
+
+        Produce a summary of past emails, listed below, between {client_company} and Bankwell Financial that will provide context most relevant to a meeting 
+        discussing {meeting_description}.
+
+        Emails:
+
+        {recent_email_str}
+     
+    """
+
+    # Create the messages to pass to the model
+    messages = [
+        HumanMessage(content=summary_prompt)
+    ]
+    
+    # Use the 'invoke' method for summarization
+    email_summary = model.invoke(messages)
+
+
+    ############# Return the new state ###############
+
+    new_state = state.copy()
+    new_state['last_meeting_timestamp'] = last_meeting_date
+    new_state['email_summary'] = email_summary.content
+
+    
+    return new_state
+
+def produce_recent_client_email_summary(state: SupervisorState) -> dict:
+    """
+        Produce a client email summary from the most recent emails based on date recieved.
+    """
+    client_company = state['client_company']
+    all_client_emails = state['all_client_emails']
+    meeting_timestamp = state['meeting_timestamp']
+    meeting_description = state['meeting_description']
+
+    last_meeting_date = find_last_meeting_date(all_client_emails=all_client_emails)
+    
+    query_str = f"""
+        SELECT email_body
+        FROM email_data
+        WHERE 
+        (
+            to_emails ~* '{'|'.join(all_client_emails)}' 
+            OR 
+            from_email ~* '{'|'.join(all_client_emails)}' 
+        )
+        order by email_timestamp desc
+        limit 4
+    """
+
+    result = pd.read_sql(query_str, con=engine)
+
+    recent_emails = list(result['email_body'])
+
+    recent_email_str = '\n'.join(recent_emails)
+
+    ###### SUMMARIZE HERE #########
+
+    summary_prompt = f"""
+        PROMPT
+
+        Produce a summary of past emails, listed below, between {client_company} and Bankwell Financial. Do not focus on logistics or meeting scheduling. Ensure you state the main topics discussed. If the {client_company} asks a question, make sure you note it. Provide a list of questions the {client_company} asked if any.
+
+        Emails:
+
+        {recent_email_str}
+     
+    """
+
+    # Create the messages to pass to the model
+    messages = [
+        HumanMessage(content=summary_prompt)
+    ]
+    
+    # Use the 'invoke' method for summarization
+    recent_email_summary = model.invoke(messages)
+
+
+    ############# Return the new state ###############
+
+    new_state = state.copy()
+    new_state['last_meeting_timestamp'] = last_meeting_date
+    new_state['recent_email_summary'] = recent_email_summary.content
+
+    
+    return new_state
+
+
+
+
+
+
